4ebe44b8a417414bc8c7f000f42881357c37417d
evgeny muravyov <e.muraviev@cti.ru>
2014-04-29 11:38:30 +0400

    -mcreated directive

--- main.js
+++ main.js
@@ -1,67 +1,86 @@
 var app = angular.module('myApp', []);
 
-app.controller('MyCtrl', MyCtrl);
-
 app.filter('offset', function() {
-  return function(input, start) {
-    start = parseInt(start, 10);
-    return input.slice(start);
-  };
+    return function(input, start) {
+        start = parseInt(start, 10);
+        return input.slice(start);
+    };
 });
 
-function MyCtrl($scope, $http) {
-    $scope.selectedItem = {};
-    $scope.data = [];
-    $scope.dataSize = 'small';
-    $scope.gridFields = [];
-    $scope.gridFieldsNames = [];
-    $scope.currentPage = 0;
-    $scope.pageSize = 30;
+app.controller('GridsManager', GridsManager);
 
-    $scope.numberOfPages=function(){
-        return Math.ceil($scope.data.length/$scope.pageSize);                
-    }
+function GridsManager($scope) {
+    $scope.count = 0
+    $scope.items = [-1];
 
+    $scope.add = function() {
+        $scope.items.push(++$scope.count)
+    };
+}
 
-    $scope.selectRow = function(row) {
-        $scope.selectedItem = JSON.stringify(row);
-    }
+app.directive('grid', function() {
+    function Grid($scope, $http) {
+        $scope.selectedItem = {};
+        $scope.data = [];
+        $scope.dataSize = 'small';
+        $scope.gridFields = [];
+        $scope.gridFieldsNames = [];
+        $scope.currentPage = 0;
+        $scope.pageSize = 30;
 
-    $http.get('http://thethz.com/dataset.php?type=small').success(function(rawData) {
-        var data = prepareData(rawData);
-
-
-        $scope.data = data;
-        if (!$scope.$$phase) {
-            $scope.$apply();
-        }
-    });
-
-    function prepareData(rawData) {
-        var data = [];
-        var row;
-
-        rawData[0].map(function(field) {
-            $scope.gridFields.push(field.field);
-            $scope.gridFieldsNames.push(field.title);
+        $scope.$watch('dataSize', function(newVal, oldVal) {
+            if (newVal !== oldVal){
+                getData();
+            }
         });
 
-        for (var i = 1, l = rawData.length; i < l; i++) {
-            row = {}
-            $scope.gridFields.map(function(fieldName, index) {
-                row[fieldName] = rawData[i][index];
-            });
-            data.push(row);
-        }
-        return data;
-    };
+        getData();
 
-}
-app.directive('mygrid', function() {
+        $scope.numberOfPages = function() {
+            return Math.ceil($scope.data.length / $scope.pageSize);
+        }
+
+        $scope.selectRow = function(row) {
+
+            $scope.selectedItem = [row.name, row.price,row.bid].join(',');
+        }
+
+        function updateGrid(data, status, headers, config) {
+            var data = prepareData(data);
+            $scope.data = data;
+        };
+
+        function getData() {
+            $http.get('http://thethz.com/dataset.php?type=' + $scope.dataSize).success(updateGrid).error(function() {
+                console.log('error - cant get data');
+            });        
+        };
+
+        function prepareData(rawData) {
+            var data = [];
+            var row;
+
+            rawData[0].map(function(field) {
+                $scope.gridFields.push(field.field);
+                $scope.gridFieldsNames.push(field.title);
+            });
+
+            for (var i = 1, l = rawData.length; i < l; i++) {
+                row = {}
+                $scope.gridFields.map(function(fieldName, index) {
+                    row[fieldName] = rawData[i][index];
+                });
+                data.push(row);
+            }
+            return data;
+        };
+
+    }
+
     return {
         restrict: 'AE',
         templateUrl: 'grid.html',
         scope: {},
-        controller: MyCtrl
+        controller: Grid
     };
 });
